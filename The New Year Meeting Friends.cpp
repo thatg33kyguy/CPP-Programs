c10c5043799aa8eddc0209a6a2a8f08c093c6bac
Rajprateem <rajprateemnath101@gmail.com>
2025-07-11 16:38:46 +0530

    new problem

---
+++ The New Year Meeting Friends.cpp
@@ -0,0 +1,53 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define all(v) v.begin(),v.end()
+
+const int N=2e5+10, inf=1e9+10;
+const int mod1=(1e9+7);
+const int mod2=(998244353);
+
+
+
+
+
+ void solve(){
+
+    int a,b,c; 
+    cin >> a >> b >> c;
+    int ans=200;
+
+    vector<int> v;
+    v.push_back(a);
+    v.push_back(b);
+    v.push_back(c);
+    sort(v.begin(), v.end());
+
+  ans= v[2]-v[0];
+
+    cout << ans << endl;
+
+
+    
+    
+
+
+    
+}
+ 
+signed main() {
+
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    
+    // int t = 1;
+    // cin >> t;
+    // while(t--) {
+ 
+        solve();
+        
+    // }
+ 
+    return 0;
+}
+
